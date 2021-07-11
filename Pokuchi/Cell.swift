e041b8791afcc6b19620ea3f3da7339fde65857d
Danny Thi <hailongdannythi@gmail.com>
2021-07-11 16:00:12 +0900

    Initial Commit.

---
+++ Pokuchi/Cell.swift
@@ -0,0 +1,91 @@
+//
+//  Cell.swift
+//  Pokuchi
+//
+//  Created by Hai Long Danny Thi on 2021/07/11.
+//
+
+import Foundation
+
+struct Cell {
+   let row: Int
+   let col: Int
+   
+   private(set) var isBomb: Bool
+   private(set) var number: Int
+   private(set) var isExposed: Bool = false
+   private(set) var isGuess: Bool = false
+   
+   init(row: Int, col: Int, isBomb: Bool, number: Int = 0) {
+      self.row = row
+      self.col = col
+      self.isBomb = isBomb
+      self.number = number
+   }
+   
+   // Returns true if its a bomb
+   mutating func flip() -> Bool {
+      self.isExposed = true
+      return !isBomb
+   }
+   
+   mutating func increaseNumber() {
+      self.number += 1
+   }
+   
+   mutating func toggleGuess() -> Bool {
+      if !isExposed {
+         isGuess.toggle()
+      }
+      return isGuess
+   }
+}
+
+
+
+
+/*
+
+ 
+ public void setRowAndColumn(int r, int c) {
+    row = r;
+    column = c;
+ }
+ 
+ public void setBomb(boolean bomb) {
+    isBomb = bomb;
+    number = -1;
+ }
+ 
+ public boolean isBlank() {
+    return number == 0;
+ }
+ 
+ 
+ @Override
+ public String toString() {
+    return getUndersideState();
+ }
+ 
+ public String getSurfaceState() {
+    if (isExposed) {
+       return getUndersideState();
+    } else if (isGuess) {
+       return "B ";
+    } else {
+       return "? ";
+    }
+ }
+ 
+ public String getUndersideState() {
+    if (isBomb) {
+       return "* ";
+    } else if (number > 0) {
+       return Integer.toString(number) + " ";
+    } else {
+       return "  ";
+    }
+ }
+ 
+ */
+
---
+++ Pokuchi/PokuchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokuchiApp.swift
+//  Pokuchi
+//
+//  Created by Hai Long Danny Thi on 2021/07/11.
+//
+
+import SwiftUI
+
+@main
+struct PokuchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
